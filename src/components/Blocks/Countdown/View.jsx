5b6a00fd05085ce0d3e64ecbff3ebb147c514718
Martin Peeters <martin.peeters@affinitic.be>
2020-06-14 12:19:49 +0200

    Implement countdown block

--- src/components/Blocks/Countdown/View.jsx
+++ src/components/Blocks/Countdown/View.jsx
@@ -3,9 +3,22 @@
  * @module components/Blocks/Countdown/Edit
  */
 import React from 'react';
+import Timer from 'react-compound-timer';
+import moment from 'moment';
 
 const View = props => {
-  return <div>CountDown</div>;
+  return <div><div>join us for</div><div>This new plone conference</div><Timer
+    initialTime={moment([2020, 11, 16]).format('x') - moment().utc().format('x')}
+    direction="backward">
+    {() => (
+        <React.Fragment>
+            <Timer.Days /> days
+            <Timer.Hours /> hours
+            <Timer.Minutes /> minutes
+            <Timer.Seconds /> seconds
+        </React.Fragment>
+    )}
+    </Timer></div>;
 };
 
 export default View;
